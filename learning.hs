299c007b7c66369e76e90788aa427b70acd39e1c
aviator41812 <44075604+aviator41812@users.noreply.github.com>
2018-10-18 10:30:19 -0700

    Add files via upload

--- learning.hs
+++ learning.hs
@@ -1,16 +1,16 @@
-replaceNth :: Int -> a -> [a] -> [a]
+replaceNth :: Double -> a -> [a] -> [a]
 replaceNth _ _ [] = []
 replaceNth n newVal (x:xs)
  | n == 0 = newVal:xs
  | otherwise = x:replaceNth (n-1) newVal xs
 
---replaceNthRow :: Int -> Int -> a -> [[a]] -> [[a]]
+--replaceNthRow :: Double -> Double -> a -> [[a]] -> [[a]]
 --replaceNthRow _ _ _ [[]] = [[]]
 --replaceNthRow row col newVal ((l:ls):xs)
 -- | row == 0 = replaceNth col newVal ls
 -- | otherwise = l:replaceNthRow (row-1) col newVal xs
 
-change_elem :: Int -> Int -> a -> [[a]] -> [[a]]
+change_elem :: Double -> Double -> a -> [[a]] -> [[a]]
 -- empty list case
 change_elem _ _ _ [] = []
 
@@ -20,8 +20,10 @@ change_elem 0 0 x ((y:ys):ls) = (x:ys):ls
 -- reduce the column until we find the element to change
 change_elem 0 col x ((y:ys):ls) = [[y]] ++ change_elem 0 (col-1) x (ys:ls)
 
-
 change_elem row col x xs =
     let row_to_replace_in = xs !! row
         modified_row = replaceNth col x row_to_replace_in
     in replaceNth row modified_row xs
+
+getVal :: Double -> Double -> [[a]] -> a
+getVal row col ls = (ls !! row) !! col
